9530c52bd0f0a4bfc6f342072597e1d30af5ff79
MajkiIbrahim <50864917+MajkiIbrahim@users.noreply.github.com>
2020-09-25 06:24:18 +0200

    Added button component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 const IndexPage = () => {
   return (
   <Container>
     <FeatureImage />
+    <Content>
+      <ContentCard 
+      date="March 22, 2020" 
+      title="Kos el gahba" 
+      excerpt="Kos el gahba bil tiz aboki" 
+      slug="/test"
+      />
+    </Content>
   </Container>
   )
 }
